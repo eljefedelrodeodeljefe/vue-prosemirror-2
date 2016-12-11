6e40f75e862d274608eaa72131b0b173df302689
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2016-12-11 13:26:08 +0100

    use ua in prod

--- examples/browserify/main.js
+++ examples/browserify/main.js
@@ -2,12 +2,27 @@
 // so you don't have to do: import Vue from 'vue/dist/vue'
 // This is done with the browser options. For the config, see package.json
 import Vue from 'vue'
+import VueAnalytics from 'vue-ua'
 import App from './App.vue'
 import ProseMirror from '../../'
 
-Vue.use(ProseMirror)
+const pkg = require('../../package.json')
 
-new Vue({ // eslint-disable-line no-new
+Vue.use(ProseMirror)
+Vue.use(VueAnalytics, {
+  appName: 'vue-prosemirror-2',
+  appVersion: pkg.version,
+  trackingId: 'UA-52604616-4',
+  debug: true
+})
+
+new Vue({ // eslint-disable-line no-new,
   el: '#app',
+  mounted () {
+    if (process.env.NODE_ENV === 'production') {
+      this.$ua.trackEvent('create', 'UA-52604616-4', 'auto')
+      this.$ua.trackEvent('send', 'pageview')
+    }
+  },
   render: (h) => h(App)
 })
